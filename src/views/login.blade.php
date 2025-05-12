107dbba3b273c9baf143390b669c67449ec2b872
Milos Cvijic <milosc@realtimetraffic.com.au>
2025-05-12 17:25:13 +0200

    Fix logo login

--- src/views/login.blade.php
+++ src/views/login.blade.php
@@ -361,7 +361,7 @@
             <div class="row">
                 <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                     <div class="login-brand">
-                        <img title="{{Session::get('appname')}}" src="{{ asset('vendor/crudbooster/assets/images/webillium_logo.png') }}" alt="logo" style="width: 100%;">
+                        <img title="{{Session::get('appname')}}" src="{{ CRUDBooster::getSetting('logo') ? asset(CRUDBooster::getSetting('logo')) : asset('vendor/crudbooster/assets/images/webillium_logo.png') }}" alt="logo" style="width: 100%;">
                     </div>
 
                     <div class="card card-primary">

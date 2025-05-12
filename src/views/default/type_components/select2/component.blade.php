95cc9d6c79ace5e89673e73100540fc67c9c6f96
Milos Cvijic <milosc@realtimetraffic.com.au>
2025-05-12 12:16:56 +0200

    Fix

--- src/views/default/type_components/select2/component.blade.php
+++ src/views/default/type_components/select2/component.blade.php
@@ -125,7 +125,7 @@
     </label>
 
         <select style='width:100%' class='form-control select2 select2-hidden-accessible' id="{{$name}}" tyle="width: 100%;" tabindex="-1" aria-hidden="true"
-                {{$required}} {{$readonly}} {!!$placeholder!!} {{$disabled}} name="{{$name}}{{($form['relationship_table'])?'[]':''}}" {{ ($form['relationship_table'])?'multiple="multiple"':'' }} >
+                {{$required}} {{$readonly}} {!!$placeholder!!} {{$disabled}} name="{{$name}}{{(isset($form['relationship_table']) && $form['relationship_table'])?'[]':''}}" {{ (isset($form['relationship_table']) && $form['relationship_table'])?'multiple="multiple"':'' }} >
             @if(isset($form['dataenum']) && $form['dataenum'])
                 <option value=''>{{cbLang('text_prefix_option')}} {{$form['label']}}</option>
                 <?php
@@ -183,9 +183,9 @@
                         // Check if $id is defined, otherwise use null
                         $id_value = isset($id) ? $id : null;
                         if ($id_value) {
-                            $foreignKey = CRUDBooster::getForeignKey($table, $form['relationship_table']);
-                            $foreignKey2 = CRUDBooster::getForeignKey($select_table, $form['relationship_table']);
-                            $value = DB::table($form['relationship_table'])->where($foreignKey, $id_value);
+                            $foreignKey = CRUDBooster::getForeignKey($table, isset($form['relationship_table']) ? $form['relationship_table'] : '');
+                            $foreignKey2 = CRUDBooster::getForeignKey($select_table, isset($form['relationship_table']) ? $form['relationship_table'] : '');
+                            $value = DB::table(isset($form['relationship_table']) ? $form['relationship_table'] : '')->where($foreignKey, $id_value);
                             $value = $value->pluck($foreignKey2)->toArray();
                         } else {
                             $value = [];
--- src/views/default/type_components/select2/component_detail.blade.php
+++ src/views/default/type_components/select2/component_detail.blade.php
@@ -31,9 +31,9 @@ if ($datatable && isset($form['relationship_table']) && $form['relationship_tabl
         // Check if $id is defined, otherwise use null
         $id_value = isset($id) ? $id : null;
         if ($id_value) {
-            $foreignKey = CRUDBooster::getForeignKey($table, $form['relationship_table']);
-            $foreignKey2 = CRUDBooster::getForeignKey($datatable_table, $form['relationship_table']);
-            $ids = DB::table($form['relationship_table'])->where($foreignKey, $id_value)->pluck($foreignKey2)->toArray();
+            $foreignKey = CRUDBooster::getForeignKey($table, isset($form['relationship_table']) ? $form['relationship_table'] : '');
+            $foreignKey2 = CRUDBooster::getForeignKey($datatable_table, isset($form['relationship_table']) ? $form['relationship_table'] : '');
+            $ids = DB::table(isset($form['relationship_table']) ? $form['relationship_table'] : '')->where($foreignKey, $id_value)->pluck($foreignKey2)->toArray();
             $tableData = DB::table($datatable_table)->whereIn('id', $ids)->pluck($datatable_field)->toArray();
         } else {
             $tableData = [];

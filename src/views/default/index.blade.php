10b4aea343f5fe5217f17d863f4ec98cef2945e6
Milos Cvijic <milosc@realtimetraffic.com.au>
2025-05-12 11:04:58 +0200

    Table

--- src/views/default/index.blade.php
+++ src/views/default/index.blade.php
@@ -146,7 +146,22 @@
 
         </div>
         <div class="box-body table-responsive no-padding">
-            @include("crudbooster::default.table")
+            @php
+                // Get the mainColor from admin_template.blade.php
+                $mainColor = '#d62828'; // Default is red
+                if(Session::get('theme_color') == 'skin-blue'){
+                    $mainColor = '#48cae4';
+                }else if(Session::get('theme_color') == 'skin-yellow'){
+                    $mainColor = '#fcbf49';
+                }else if(Session::get('theme_color') == 'skin-green'){
+                    $mainColor = '#80ed99';
+                }else if(Session::get('theme_color') == 'skin-red'){
+                    $mainColor = '#d62828';
+                }else if(Session::get('theme_color') == 'skin-yellow'){
+                    $mainColor = '#003049';
+                }
+            @endphp
+            @include("crudbooster::default.table", ['mainColor' => $mainColor])
         </div>
     </div>
 
